d161fe40eab181176d7b9ad5322451d77942dd1a
Maverick JS <mavjs01@gmail.com>
2012-11-30 21:54:12 +0800

    probably this works now

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ def main():
     apu = aputime.apu('UC2F1208IT{ISS}', '2012-11-26') 
     #change the intake to see how the script works
     print apu.to_ics()
+    print apu.to_html()
 
 if __name__ == '__main__':
     main()
