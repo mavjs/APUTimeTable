c4d1b7739a26a8c3b6422eed9cc786ad642f291f
Maverick JS <mavjs01@gmail.com>
2013-06-03 21:50:18 +0800

    test file for .html, ics doesn't work yet.

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
 from apu import aputime
 
 def main():
-    apu = aputime.APU('UC2F1208IT{ISS}', '2012-11-26') 
+    apu = aputime.APU('UC2F1208IT{ISS}', '2013-06-03') 
     #change the intake to see how the script works
-    print apu.to_ics()
     print apu.to_html()
 
 if __name__ == '__main__':
